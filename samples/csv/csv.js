d044eb06651bc21c9483c1bba30327aeeb052ae1
Bruno Borges <bruno.borges@gmail.com>
2015-04-10 08:40:24 -0300

    Update csv.js

--- samples/csv/csv.js
+++ samples/csv/csv.js
@@ -6,11 +6,11 @@ with (commons_csv) {
   var charset = Charset.forName("US-ASCII");
   var file = Paths.get("sample.csv").toFile();
   var format = CSVFormat.EXCEL.withHeader().withIgnoreSurroundingSpaces();
-	var records = CSVParser.parse(file, charset, format);
-	for each (record in records) {
-      var _date = record.get("Date");
-	    var _close = record.get("Close");
-	    var _volume = record.get("Volume");
-	    print("Date: ${_date}, Close: ${_close}, Volume: ${_volume}");
-	}
+  var records = CSVParser.parse(file, charset, format);
+  for each (record in records) {
+    var _date = record.get("Date");
+    var _close = record.get("Close");
+    var _volume = record.get("Volume");
+    print("Date: ${_date}, Close: ${_close}, Volume: ${_volume}");
+  }
 }
