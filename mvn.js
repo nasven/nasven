9e0efa3f5e5b1bf28b161662be49b36de3fe51c9
Bruno Borges <bruno.borges@gmail.com>
2015-04-13 11:53:15 -0300

    ignore dependencies if not present. throw error if main not defined. ignore options if not defined

--- mvn.js
+++ mvn.js
@@ -41,16 +41,19 @@ $ENV.PWD = parentPath.toString();
 load(mvnDefFile.toString());
 
 if (typeof maven === 'undefined') {
-    print("ERROR: Nashorn Maven application has no 'maven' object defined.");
-    exit();
+  print("ERROR: Nashorn Maven application has no 'maven' object defined.");
+  exit();
+} else if (typeof maven.main === 'undefined' || maven.main === '') {
+  print("ERROR: Your 'maven' object in your ${arguments[0]} config file does not define 'main'.")
+  exit();
 }
 
-var mainScript = Paths.get(parentPath, maven.main);
+var mainScript = Paths.get(parentPath, maven.main).toAbsolutePath();
 checkPathExists(mainScript);
 
-var dependenciesCP = '';
+var classpath = '';
 if (typeof maven.dependencies !== 'undefined') {
-  dependenciesCP = Arrays.stream(Java.to(maven.dependencies, "java.lang.String[]"))
+  var dependenciesCP = Arrays.stream(Java.to(maven.dependencies, "java.lang.String[]"))
     .map(function (dep) {return dep.split(":");})
     .map(function (tokens) {
       return <<EOF
@@ -62,33 +65,34 @@ if (typeof maven.dependencies !== 'undefined') {
       EOF
     })
     .collect(Collectors.joining("\n"));
+
+  var pomTemplate = <<EOF
+   <project>
+      <modelVersion>4.0.0</modelVersion>
+      <groupId>nashorn-maven</groupId>
+      <artifactId>nashorn-maven-temp-artifact</artifactId>
+      <packaging>pom</packaging>
+      <version>1.0-SNAPSHOT</version>
+      <name>nashorn-maven-temp-artifact</name>
+      <dependencies>
+        ${dependenciesCP}
+      </dependencies>
+   </project>
+  EOF
+
+  var pomFile = Files.createTempFile('pom-', '-' + mainScript.toFile().getName() + '.xml').toAbsolutePath();
+  Files.write(pomFile, pomTemplate.getBytes());
+
+  var cpFile = Paths.get(parentPath, mainScript.toFile().getName() + '.cp').toAbsolutePath();
+  $EXEC("mvn -f ${pomFile} -Dmdep.outputFile=${cpFile} dependency:build-classpath");
+  classpath = new jString('-cp ' + Files.readAllBytes(cpFile));
+  Files.delete(cpFile);
 }
-
-var pomTemplate = <<EOF
- <project>
-    <modelVersion>4.0.0</modelVersion>
-    <groupId>nashorn-maven</groupId>
-    <artifactId>nashorn-maven-temp-artifact</artifactId>
-    <packaging>pom</packaging>
-    <version>1.0-SNAPSHOT</version>
-    <name>nashorn-maven-temp-artifact</name>
-    <dependencies>
-      ${dependenciesCP}
-    </dependencies>
- </project>
-EOF
-
-var pomFile = Files.createTempFile('pom-', '-' + mainScript.toFile().getName() + '.xml').toAbsolutePath();
-Files.write(pomFile, pomTemplate.getBytes());
-
-var cpFile = Paths.get(parentPath, mainScript.toFile().getName() + '.cp').toAbsolutePath();
-$EXEC("mvn -f ${pomFile} -Dmdep.outputFile=${cpFile} dependency:build-classpath");
-var classpath = new jString(Files.readAllBytes(cpFile));
-Files.delete(cpFile);
 
 arguments.shift();
 newargs = arguments.join(" ");
 
-$EXEC("jjs -cp ${classpath} ${maven.options} ${maven.main} -- ${newargs}");
+var options = typeof maven.options === 'undefined' ? '' : maven.options;
+$EXEC("jjs ${classpath} ${options} ${mainScript} -- ${newargs}");
 print($OUT);
 print($ERR);
