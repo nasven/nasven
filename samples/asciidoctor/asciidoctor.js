83658d38d175c155bd99cabaa7faffe061f66088
Bruno Borges <bruno.borges@gmail.com>
2015-04-13 18:35:16 -0300

    fixed import asciidoc sampe

--- samples/asciidoctor/asciidoctor.js
+++ samples/asciidoctor/asciidoctor.js
@@ -2,7 +2,7 @@ var Paths = Packages.java.nio.file.Paths;
 var Files = Packages.java.nio.file.Files;
 var Factory = Packages.org.asciidoctor.Asciidoctor.Factory;
 var Asciidoctor = Packages.org.asciidoctor.Asciidoctor;
-var OptionsBuilder = org.asciidoctor.OptionsBuilder;
+var OptionsBuilder = Packages.org.asciidoctor.OptionsBuilder;
 
 var sample = new java.lang.String(Files.readAllBytes(Paths.get("mysample.adoc")));
 
