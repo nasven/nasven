f59037e65b6adcb7b0713364eadc864548918746
Bruno Borges <bruno.borges@gmail.com>
2015-04-13 16:06:24 -0300

    prints output on request of ratpack

--- samples/ratpack/ratpack.js
+++ samples/ratpack/ratpack.js
@@ -2,8 +2,14 @@ var RatpackServer = Packages.ratpack.server.RatpackServer;
 
 RatpackServer.start(function (server) { server 
   .handlers(function (chain) { chain
-    .get(function (ctx) {ctx.render("Hello World!")})
-    .get(":name", function (ctx) {ctx.render("Hello " + ctx.getPathTokens().get("name") + "!")})
+    .get(function (ctx) {
+      print('Rendering hello world');
+      ctx.render("Hello World!")
+    })
+    .get(":name", function (ctx) {
+      print('Rendering hello ${ctx.getPathTokens().get("name")}');
+      ctx.render("Hello " + ctx.getPathTokens().get("name") + "!")
+    })
   })
 });
 
