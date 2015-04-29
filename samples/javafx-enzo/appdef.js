520daf7daed7a4de0183dc6d7960a417f9a5047c
Bruno Borges <bruno.borges@gmail.com>
2015-04-29 11:29:38 -0300

    new JavaFX sample using Enzo

---
+++ samples/javafx-enzo/appdef.js
@@ -0,0 +1,11 @@
+/**
+ * This is an example of a Nashorn Maven application.
+ *
+ * Author: Bruno Borges (@brunoborges)
+ * Since: 2015
+ */
+var appdef = {
+  main: "enzo.js",
+  options: "-fx",
+  dependencies: ["eu.hansolo.enzo:Enzo:0.2.4"]
+}
