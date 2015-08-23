07d63fc1e2f3eee28cf540e0514f83014e240cf6
Bruno Borges <bruno.borges@gmail.com>
2015-08-23 20:03:39 -0300

    upgrade works now

--- nasven.js
+++ nasven.js
@@ -30,6 +30,7 @@ if (arguments[0] === 'upgrade') {
   print("[NASVEN] Upgrading Nasven.js ... ");
   var NASVEN_DIR=$ENV.HOME + "/.nasven";
   print("[NASVEN] Downloading latest Nasven.js from GitHub ... ");
+  $EXEC("rm ${NASVEN_DIR}/nasven*");
   $EXEC("curl -sSL https://raw.githubusercontent.com/nasven/nasven/master/nasven -o ${NASVEN_DIR}/nasven");
   $EXEC("curl -sSL https://raw.githubusercontent.com/nasven/nasven/master/nasven.js -o ${NASVEN_DIR}/nasven.js");
   $EXEC("chmod +x ${NASVEN_DIR}/nasven*");
